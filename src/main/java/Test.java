b808f138a5ba61d82a0d04d27fcb790f9f5a330a
huazi <huaziqing@163.com>
2018-11-14 23:15:58 +0800

    second commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @program: Test
+ * @description: 测试
+ * @author: huazi
+ * @version: 1.0
+ * @create: 2018-11-14 23:14
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("helloWorld!");
+    }
+}
